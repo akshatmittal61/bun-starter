131c1b034c7fe661d13de62c37ed982759f017f5
Akshat Mittal <akshatmittal2506@gmail.com>
2023-11-01 10:52:21 +0530

    server config

--- index.ts
+++ index.ts
@@ -3,7 +3,46 @@ import { PORT } from "./config";
 const server = Bun.serve({
 	port: PORT,
 	fetch(request, server) {
-		return new Response("Hello, World!");
+		const url = new URL(request.url);
+		switch (url.pathname) {
+			case "/":
+				return new Promise((resolve, reject) => {
+					const html = `
+                        <html>
+                            <head>
+                                <title>Bun.js</title>
+                            </head>
+                            <body>
+                                <h1>Hello, world!</h1>
+                            </body>
+                        </html>
+                    `;
+					resolve(new Response(html, { status: 200 }));
+				});
+			default:
+				return new Promise((resolve, reject) => {
+					const html = `
+                        <html>
+                            <head>
+                                <title>Bun.js</title>
+                            </head>
+                            <body>
+                                <h1>404 Not Found</h1>
+                            </body>
+                        </html>
+                    `;
+					resolve(new Response(html, { status: 404 }));
+				});
+				break;
+		}
+	},
+	websocket: {
+		open(socket) {
+			console.log("Socket opened");
+		},
+		message(socket, message) {
+			console.log("Socket message:", message);
+		},
 	},
 });
 
